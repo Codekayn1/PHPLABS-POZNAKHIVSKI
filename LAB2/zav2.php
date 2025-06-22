e5f1c8ad6db3346b23e0a8be396bd26cd2b1cbbc
Codekayn1 <kn1b22.poznakhivskyi@kpnu.edu.ua>
2025-06-22 14:47:17 +0300

    Create zav2.php

---
+++ LAB2/zav2.php
@@ -0,0 +1,16 @@
+<?php
+$intNumber = 10;
+
+$floatNumber = 3.75;
+
+$convertedToInt = (int)$floatNumber;
+
+$convertedToFloat = (float)$intNumber;
+
+
+echo "Ціле число: " . $intNumber . "<br>";
+echo "Дробове число: " . $floatNumber . "<br>";
+echo "Дробове число, явно перетворене в ціле: " . $convertedToInt . "<br>";
+echo "Ціле число, явно перетворене в дробове: " . $convertedToFloat . "<br>";
+?>
+
